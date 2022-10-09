547cebbce37dcbc1c94042c8f3380e11ac0fd734
yusei88 <yuseisato1@gmail.com>
2022-10-09 16:43:28 +0900

    スタート画面表示の実装 #1

---
+++ lib/view/start_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'スライドパズル',
+            style: TextStyle(fontSize: 32),
+          ),
+          const SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () => {},
+            child: const Text('スタート'),
+          )
+        ],
+      ),
+    ));
+  }
+}
